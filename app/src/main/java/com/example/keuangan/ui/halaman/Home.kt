63bdcb80c906b1c4b274634d2b3288cc4f237f07
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2024-01-01 15:38:57 +0700

    membuat floating action button di halaman home

--- app/src/main/java/com/example/keuangan/ui/halaman/Home.kt
+++ app/src/main/java/com/example/keuangan/ui/halaman/Home.kt
@@ -5,55 +5,116 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.keuangan.R
 import com.example.keuangan.nav.Screens
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home(
     navController: NavController,
 ){
-    Column(
-        modifier = Modifier
-            .padding(start = 50.dp, end = 50.dp)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        // get user data Button
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = {
-                navController.navigate(route = Screens.GetDataScreen.route)
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Home") },
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    navController.navigate(route = Screens.AddDataScreen.route)
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.entity_data)
+                )
             }
-        ) {
-            Text(text = "Get All Data")
-        }
-
-        // add user data Button
-        OutlinedButton(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = {
-                navController.navigate(route = Screens.AddDataScreen.route)
-            }
-        ) {
-            Text(text = "Add Pemasukan")
-        }
-
-        OutlinedButton(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = {
-                navController.navigate(route = Screens.AddPengeluaran.route)
-            }
-        ) {
-            Text(text = "Add Pengeluaran")
-        }
+        },
+    ) { innerPadding ->
+        BodyHome(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+           )
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@Composable
+fun BodyHome(
+    modifier: Modifier,
+    ) {
+//    Column(
+//        modifier = Modifier
+//            .padding(start = 50.dp, end = 50.dp)
+//            .fillMaxSize(),
+//        verticalArrangement = Arrangement.Center,
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//        // get user data Button
+//        Button(
+//            modifier = Modifier.fillMaxWidth(),
+//            onClick = {
+//                navController.navigate(route = Screens.GetDataScreen.route)
+//            }
+//        ) {
+//            Text(text = "Get All Data")
+//        }
+//
+//        // add user data Button
+//        OutlinedButton(
+//            modifier = Modifier.fillMaxWidth(),
+//            onClick = {
+//                navController.navigate(route = Screens.AddDataScreen.route)
+//            }
+//        ) {
+//            Text(text = "Add Pemasukan")
+//        }
+//
+//        OutlinedButton(
+//            modifier = Modifier.fillMaxWidth(),
+//            onClick = {
+//                navController.navigate(route = Screens.AddPengeluaran.route)
+//            }
+//        ) {
+//            Text(text = "Add Pengeluaran")
+//        }
+//    }
 }
